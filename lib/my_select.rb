21755fb086074ffba6fcd959ce214c9aa51b71e5
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-05 19:08:00 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,16 @@
 def my_select(collection)
-   i = 0
-   new_collection = []
+  i = 0
+  boolean_collection = []
   while (i < collection.length) do 
-    new_collection.push(yield collection[i])
+    boolean_collection.push(yield collection[i])
     i += 1 
   end 
+  l = 0 
+  new_collection = []
+  while (l < boolean_collection.length) do
+    if boolean_collection[l] == true 
+      new_collection.push(boolean_collection[l])
+    end
+  end
   new_collection 
 end
