fa02db1a0399b3bab876b4d005267ec89be79a33
Kirsten Werner <kirstenmwerner@gmail.com>
2019-02-05 19:00:23 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
    i = 0
    new_collection = []
   while (i < collection.length) do 
-    new_collection.push(yield array[i])
+    new_collection.push(yield collection[i])
     i += 1 
   end 
   new_collection 
